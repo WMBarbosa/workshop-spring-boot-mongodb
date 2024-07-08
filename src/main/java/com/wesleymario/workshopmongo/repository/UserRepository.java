1bba393b61950a72da675c1bbf5ff1b4b33d49e6
WMBarbosa <wesleymario01@gmail.com>
2024-07-08 15:35:04 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/wesleymario/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wesleymario.workshopmongo.repository;
+
+import com.wesleymario.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/wesleymario/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.wesleymario.workshopmongo.services;
+
+
+import com.wesleymario.workshopmongo.domain.User;
+import com.wesleymario.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+}
